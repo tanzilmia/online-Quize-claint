f7e4302bd611817463a32bb3eec1e7711df8b6ac
tanzilmia <ziltan590@gmail.com>
2023-01-21 21:08:54 +0600

    insert data

--- src/AdminPanel/AddQuize.js
+++ src/AdminPanel/AddQuize.js
@@ -1,11 +1,123 @@
-import React from 'react';
-
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import '../cssFiles/AddQuize.css'
 const AddQuize = () => {
-    return (
-        <div>
-            <h2>Add Quize Page</h2>
-        </div>
-    );
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+      } = useForm();
+
+      const [quizeError, setquizeError] = useState("");
+
+      const handleAddQuize = (data) =>{
+        setquizeError("")
+        const title = data.title;
+        const optionOne= data.optionOne;
+        const optionTow = data.optionTow;
+        const optionThree = data.optionThree;
+        const optionFour = data.optionFour;
+        const categoryName = data.categoryName;
+        const correctAnswer = data.correctAnswer;
+
+        const quizeData = {
+            title,
+            quizeOptions : [optionOne,optionTow,optionThree,optionFour],
+            correctAnswer,
+            categoryName
+
+        }
+        
+        // post quize 
+
+      try{
+        fetch(`http://localhost:5000/insertquize`,{
+            method: 'POST',
+            headers: {
+                'content-type':'application/json'
+            },
+            body:JSON.stringify(quizeData)
+        })
+        .then(res=> res.json())
+        .then(data =>{
+            console.log(data);
+            if(data.message === "exist"){
+                alert("Quize is already exist")
+            }
+            if(data.message === "successfull"){
+                alert("Quize add successfull")
+            }
+        })
+      }catch(error){
+        console.log(error)
+      }
+      }
+
+
+
+  return (
+   <div>
+    <h2>Insert New Quize</h2>
+
+    <div className="w-8/12 mx-auto p-5 form">
+        <h2 className="text-center text-3xl font-bold">Insert New Quize</h2>
+        <form onSubmit={handleSubmit(handleAddQuize)} className="form_inside">
+            <div className="title">
+                <input type="text"  {...register("title", {
+                required: "Name is Required",
+              })} placeholder="Add Quize Title" />
+            </div>
+            <div className="options grid grid-cols-2 gap-2 ">
+                <div className="option">
+                <input type="text"  {...register("optionOne", {
+                required: "Name is Required",
+              })} placeholder="option 1" />
+                </div>
+                <div className="option">
+                <input type="text"  {...register("optionTow", {
+                required: "Name is Required",
+              })} placeholder="option 2" />
+                </div>
+                <div className="option">
+                <input type="text"  {...register("optionThree", {
+                required: "Name is Required",
+              })} placeholder="option 3" />
+                </div>
+                <div className="option">
+                <input type="text"  {...register("optionFour", {
+                required: "Name is Required",
+              })} placeholder="option 4" />
+                </div>
+                
+                
+            </div>
+           
+            <div className="grid grid-cols-2 gap-2 mt-5">
+            <div className="option">
+           <input type="text"  {...register("correctAnswer", {
+                required: "Name is Required",
+              })} placeholder="Inter Correct Anser" />
+           </div>
+           
+            <div className="select_categorys">
+               
+               <select name="select categorys"  {...register("categoryName", {
+                required: "Name is Required",
+              })} id="">
+                   <option value="insert Quize">Insert Quize</option>
+                   <option value="2">2</option>
+                   <option value="3">3</option>
+               </select>
+           </div>
+            </div>
+            <div className="quize_btn">
+                <button type="submit">Click To Insert</button>
+            </div>
+        </form>
+    </div>
+
+   </div>
+  );
 };
 
 export default AddQuize;
