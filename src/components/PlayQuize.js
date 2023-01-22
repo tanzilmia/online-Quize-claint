56230378d555880d9cf0adfe7001f27dade13b10
tanzilmia <ziltan590@gmail.com>
2023-01-22 21:03:56 +0600

    trying to add timer

--- src/components/PlayQuize.js
+++ src/components/PlayQuize.js
@@ -1,15 +1,71 @@
-import React from 'react';
-import { useLoaderData } from 'react-router-dom';
-
+import React, { useEffect, useState } from "react";
+import { useLoaderData } from "react-router-dom";
+import '../cssFiles/PlayQuize.css'
 const PlayQuize = () => {
-    const quizeData = useLoaderData()
-    console.log(quizeData)
-    const {categoryName} = quizeData?.data[0]
-    return (
-        <div>
-            <h2>playing Quize {categoryName} for Free   </h2> 
-        </div>
-    );
+  const [currentQuize, setcurrentQuize] = useState(0);
+  const [currentQuestion, setcurrentQuestion] = useState(0);
+  const [stop, setStop] = useState(false);
+  const time = 10
+  const [remainingTime, setRemainingTime] = useState(3);
+  const quizeData = useLoaderData();
+
+  console.log(quizeData);
+  const quizes = quizeData?.data;
+  const answer = quizes[currentQuestion]?.correctAnswer;
+
+//   useEffect(() => {
+//     const interval = setInterval(() => {
+//         if (remainingTime === 0) {
+//             // call function to stop quiz
+//             setRemainingTime(4)
+//             setcurrentQuestion(currentQuestion + 1);
+             
+//             return;
+//           }
+//           setRemainingTime(remainingTime - 1);
+          
+//     }, 1000);
+    
+//     if (stop)
+//     return () => clearInterval(interval);
+    
+//   }, [remainingTime,currentQuestion,stop]);
+
+  // quize answer gettting
+  const submitQuize = (option) => {
+    if (option === answer) {
+      alert("right answer");
+      setcurrentQuestion(currentQuestion + 1);
+    }
+    if (option !== answer) {
+      alert("wrong answer");
+      setcurrentQuestion(currentQuestion + 1);
+    }
+  };
+
+  return (
+    <div className="w-6/12 mx-auto text-center mt-11">
+      <div>Remaining Time: {remainingTime} seconds</div>
+      <h2 className="text-xl text-center">{quizes[currentQuestion]?.title} </h2>
+      <ul className="Option grid grid-cols-2 gap-2 mt-4">
+        {quizes[currentQuestion]?.quizeOptions?.map((option, _id) => (
+          <>
+            
+            
+              <button
+                className="option"
+                key={_id}
+                onClick={() => submitQuize(option)}
+              >
+                {" "}
+                {option}{" "}
+              </button>
+            
+          </>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default PlayQuize;
